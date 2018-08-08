4aeeeecf1aec737c485e9cce535df594f46e09a6
eloisa froes <ola.eloisa@gmail.com>
2018-08-08 15:51:57 -0300

    created review controller and updated routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
+
   belongs_to :restaurant
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: 2572 7285,
+    category:     'Indian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: 2609 8522,
+    category:     'Pizza'
+  },
+  {
+    name:         'Ferro & Farinha',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: 2572 7285,
+    category:     'Pizza'
+  },
+  {
+    name:         'Miam Miam',
+    address:      'Botafogo, Rio de Janeiro E1 6PQ',
+    phone_number: 2609 8522,
+    category:     'Contemporary'
+  },
+  {
+    name:         'Hachiko',
+    address:      'Centro, Rio de Janeiro E1 6PQ',
+    phone_number: 2609 8522,
+    category:     'Japanese'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
